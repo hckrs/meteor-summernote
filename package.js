7e3c804df072171b060ba4b42d42d55deb396564
Jarno Le Conté <jarno.leconte@me.com>
2014-10-14 16:47:14 +0200

    Meteor package server required to specify package numbers

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.addFiles('summernote/dist/summernote.js', 'client');
   api.addFiles('summernote/dist/summernote.css', 'client');
-  api.use('jquery', 'client');
-  api.use('mizzao:bootstrap-3', 'client');
-  api.use('chrismbeckett:fontawesome4', 'client');
+  api.use('jquery@1.0.0', 'client');
+  api.use('mizzao:bootstrap-3@3.2.0', 'client');
+  api.use('chrismbeckett:fontawesome4@4.2.1', 'client');
 });
